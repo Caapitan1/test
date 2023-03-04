211d8016337f4afc1600fe97ef2107021e84b7f8
Caapitan1 <agads87@gmail.com>
2023-03-04 15:02:37 +0300

    test file

--- test/index.php
+++ test/index.php
@@ -1,8 +1,3 @@
 <?php
 
-require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
-
-global $USER;
-echo "[".$USER->GetID()."] (".$USER->GetLogin().") ".$USER->GetFullName();
-
 echo 123;
